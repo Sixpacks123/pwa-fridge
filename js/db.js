4205db8b6a3f8f50118197747ae6231f1ff8023d
Heurtault Aubin <aub.heurtault@gmail.com>
2024-04-05 13:33:34 +0200

    Refactor addProduct function and displayProducts function

--- js/db.js
+++ js/db.js
@@ -4,9 +4,8 @@ const request = indexedDB.open('productsDB', 1);
 request.onupgradeneeded = function(event) {
     db = event.target.result;
     if (!db.objectStoreNames.contains('products')) {
-        let store = db.createObjectStore('products', { keyPath: 'id', autoIncrement:true });
-        store.createIndex('name', 'name', { unique: false });
-        store.createIndex('expirationDate', 'expirationDate', { unique: false });
+        db.createObjectStore('products', { keyPath: 'id', autoIncrement:true })
+          .createIndex('expirationDate', 'expirationDate', { unique: false });
     }
 };
 
@@ -16,48 +15,43 @@ request.onsuccess = function(event) {
     displayProducts();
 };
 
-request.onerror = function(event) {
-    console.error('Error opening DB', event);
-};
-
-function addProduct(name, date) {
-    let transaction = db.transaction(['products'], 'readwrite');
-    let store = transaction.objectStore('products');
-    let item = { name, expirationDate: date };
-    store.add(item);
-
-    transaction.oncomplete = () => {
-        console.log('Product added');
-        displayProducts();
-    };
+function addProductToDB(name, date) {
+    return new Promise((resolve, reject) => {
+        let transaction = db.transaction(['products'], 'readwrite');
+        let store = transaction.objectStore('products');
+        store.add({ name, expirationDate: date }).onsuccess = () => {
+            console.log('Product added to database');
+            displayProducts();
+            resolve();
+        };
+    });
 }
 
 function displayProducts() {
-    const now = new Date();
+    let now = new Date();
     let objectStore = db.transaction('products').objectStore('products');
-    document.getElementById('productList').innerHTML = '';  
+    document.getElementById('productList').innerHTML = '';
+    
     objectStore.openCursor().onsuccess = function(event) {
         let cursor = event.target.result;
         if (cursor) {
             const { name, expirationDate } = cursor.value;
-            const expDate = new Date(expirationDate);
-            const listItem = document.createElement('li');
-            listItem.textContent = `Product: ${name}, Expiration Date: ${expirationDate}`;
-            document.getElementById('productList').appendChild(listItem);
-
-            const timeDiff = expDate.getTime() - now.getTime();
-            const daysToExpire = Math.ceil(timeDiff / (1000 * 3600 * 24));
-
-            if (daysToExpire < 0) {
-                notifyUser(`${name} has expired!`);
-            } else if (daysToExpire <= 3) {
-                notifyUser(`${name} will expire in ${daysToExpire} day(s)!`);
-            }
-
+            displayProductItem(name, expirationDate, now);
             cursor.continue();
-        } else {
-            console.log('No more products to display.');
         }
     };
 }
 
+function displayProductItem(name, expirationDate, now) {
+    const expDate = new Date(expirationDate);
+    const listItem = document.createElement('li');
+    listItem.textContent = `Product: ${name}, Expiration Date: ${expirationDate}`;
+    document.getElementById('productList').appendChild(listItem);
+
+    const daysToExpire = (expDate - now) / (1000 * 3600 * 24);
+    if (daysToExpire < 0) {
+        notifyUser(`${name} has expired!`);
+    } else if (daysToExpire <= 3) {
+        notifyUser(`${name} will expire soon!`);
+    }
+}
