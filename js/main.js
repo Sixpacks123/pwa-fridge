481bb1c092041423de1689ad698fab50e80a4759
Heurtault Aubin <aub.heurtault@gmail.com>
2024-04-05 11:19:47 +0200

    Update service worker registration paths

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        navigator.serviceWorker.register('sw.js').then(function(registration) {
             console.log('Service Worker Registered', registration);
         }).catch(function(err) {
             console.log('Service Worker Failed to Register', err);
