112d94a2c30d87f442a6a4e1ee76564bebbbdd42
Heurtault Aubin <aub.heurtault@gmail.com>
2024-04-05 13:57:04 +0200

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -7,13 +7,20 @@ const filesToCache = [
   '/icons/frigo-64.png',
   '/icons/frigo-512x512.png',
 ];
+
 self.addEventListener("install", (e) => {
-    console.log("[SW] install");
-    e.waitUntil((async () => {
-        const cache = await caches.open(cacheName);
-        console.log("[SW] Caching files");
-        await cache.addAll(filesToCache);
-    })());
+    console.log("[SW] Install");
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            const cachePromises = filesToCache.map(url => {
+                return cache.add(url).catch(err => console.error(`Caching failed for ${url}: ${err}`));
+            });
+            return Promise.all(cachePromises).then(() => {
+                console.log("[SW] All specified resources have been cached.");
+                return self.skipWaiting(); 
+            });
+        }).catch(err => console.error("Failed to open cache", err))
+    );
 });
 
 
