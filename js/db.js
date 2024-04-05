090e11063eaab5e84f3005bd1f4f86a4a7896206
Heurtault Aubin <aub.heurtault@gmail.com>
2024-04-05 11:46:21 +0200

    Add expiration notifications to displayProducts function

--- js/db.js
+++ js/db.js
@@ -33,16 +33,48 @@ function addProduct(name, date) {
 }
 
 function displayProducts() {
+    let now = new Date();
     let objectStore = db.transaction('products').objectStore('products');
-    document.getElementById('productList').innerHTML = ''; 
+    document.getElementById('productList').innerHTML = '';
+
     objectStore.openCursor().onsuccess = function(event) {
         let cursor = event.target.result;
         if (cursor) {
             const { name, expirationDate } = cursor.value;
             const listItem = document.createElement('li');
+            let expDate = new Date(expirationDate);
             listItem.innerHTML = `Product: ${name}, Expiration Date: ${expirationDate}`;
             document.getElementById('productList').appendChild(listItem);
+
+            // Check for expiration and near expiration
+            let daysToExpire = (expDate - now) / (1000 * 3600 * 24);
+            if (daysToExpire < 0) {
+                notifyUser(`${name} has expired!`);
+            } else if (daysToExpire < 3) {
+                notifyUser(`${name} will expire soon!`);
+            }
+
             cursor.continue();
         }
     };
+}
+
+
+function notifyUser(message) {
+    if (!("Notification" in window)) {
+        console.log("This browser does not support notifications.");
+        return;
+    }
+
+    if (Notification.permission === "granted") {
+        new Notification(message);
+    }
+
+    else if (Notification.permission !== 'denied' || Notification.permission === "default") {
+        Notification.requestPermission().then(function (permission) {
+            if (permission === "granted") {
+                new Notification(message);
+            }
+        });
+    }
 }
