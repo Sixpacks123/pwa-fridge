a7d8b513cb7750d0c2b932dd049f25be1656029e
Heurtault Aubin <aub.heurtault@gmail.com>
2024-04-17 14:02:16 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,50 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Service Worker Registration
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('sw.js').then(function(registration) {
+        navigator.serviceWorker.register('sw.js').then(registration => {
             console.log('Service Worker Registered', registration);
-        }).catch(function(err) {
-            console.log('Service Worker Failed to Register', err);
+            setupPeriodicSync(registration);
+        }).catch(err => {
+            console.error('Service Worker Failed to Register', err);
         });
     }
 
     // Notification Permission Request
     if ('Notification' in window) {
         Notification.requestPermission().then(permission => {
-            if (permission === "granted") {
-                console.log("Notification permission granted.");
-            } else {
-                console.log("Notification permission not granted.");
-            }
+            console.log(permission === "granted" ? "Notification permission granted." : "Notification permission not granted.");
         });
     }
 
-    navigator.serviceWorker.ready.then(function(registration) {
-        if ('periodicSync' in registration) {
-            try {
-                registration.periodicSync.register('content-sync', {
-                    minInterval: 60 * 1000,
-                });
-            } catch (error) {
-                console.error(`Periodic Sync could not be registered!`, error);
-            }
-        }
-    });
     // Form Submission Handling
-    document.getElementById('productForm').addEventListener('submit', function(event) {
+    const form = document.getElementById('productForm');
+    form.addEventListener('submit', function(event) {
         event.preventDefault();
         const name = document.getElementById('productName').value;
         const date = document.getElementById('expirationDate').value;
         addProduct(name, date);
-        document.getElementById('productForm').reset(); // Reset form fields after submission
+        form.reset(); // Reset form fields after submission
     });
 });
 
-
-function addProduct(name, date) {
-    addProductToDB(name, date).then(() => {
+async function addProduct(name, date) {
+    if (typeof addProductToDB === 'function') {
+        await addProductToDB(name, date);
         console.log('Product added to local database');
-    });
+    }
 }
 
 async function setupPeriodicSync(registration) {
