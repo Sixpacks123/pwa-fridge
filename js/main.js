329422e061fdf9a631162cf231cdd72a516ed3f3
Heurtault Aubin <aub.heurtault@gmail.com>
2024-05-03 09:30:31 +0200

    Fix service worker registration in main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Service Worker Registration
-    if ('serviceWorker' in navigator && 'serviceWorker' in navigator) {
+    if ('serviceWorker' in navigator && navigator.serviceWorker) {
         navigator.serviceWorker.register('sw.js').then(registration => {
             console.log('Service Worker Registered', registration);
             setupPeriodicSync(registration);
